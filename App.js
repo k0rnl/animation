95d4de204c6d671ce18f90f206f8be3b6b420c4c
Kornel Kocsis <kornel.kocsis@cometstudio.io>
2023-05-09 11:40:12 +0200

    add animation

--- App.js
+++ App.js
@@ -1,20 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { Button, SafeAreaView } from "react-native";
+import { FadeIn, BounceOut } from "react-native-reanimated";
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withSpring,
+} from "react-native-reanimated";
+import * as Haptics from "expo-haptics";
 
-export default function App() {
+function Box() {
+  const offset = useSharedValue(0);
+  const [showButton, setShowButton] = React.useState(false);
+
+  const animatedStyles = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateX: offset.value * 255 }],
+    };
+  });
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <Animated.View
+        style={[
+          { width: 100, height: 100, backgroundColor: "green" },
+          animatedStyles,
+        ]}
+      />
+      <Button
+        title="Start animations"
+        onPress={() => {
+          Haptics.notificationAsync(Haptics.NotificationFeedbackType.Success);
+          offset.value = withSpring(Math.random());
+          setShowButton(!showButton);
+        }}
+      />
+      {showButton ? (
+        <Animated.View
+          style={[
+            {
+              marginTop: 10,
+              width: 100,
+              height: 100,
+              backgroundColor: "green",
+              alignSelf: "center",
+            },
+          ]}
+          entering={FadeIn.duration(300)}
+          exiting={BounceOut.duration(450)}
+        />
+      ) : null}
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  return (
+    <SafeAreaView>
+      <Box />
+    </SafeAreaView>
+  );
+}
